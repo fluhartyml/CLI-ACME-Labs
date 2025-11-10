eec22293e29c83e9f79a4d4061c1bd91f75deaf9
Michael Fluharty <michael.fluharty@mac.com>
2025-11-10 09:49:49 -0600

    Initial Commit

---
+++ CLI ACME Labs/CLI_ACME_LabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLI_ACME_LabsApp.swift
+//  CLI ACME Labs
+//
+//  Created by Michael Fluharty on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CLI_ACME_LabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CLI ACME LabsTests/CLI_ACME_LabsTests.swift
@@ -0,0 +1,16 @@
+//
+//  CLI_ACME_LabsTests.swift
+//  CLI ACME LabsTests
+//
+//  Created by Michael Fluharty on 11/10/25.
+//
+
+import Testing
+
+struct CLI_ACME_LabsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
